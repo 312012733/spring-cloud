063113cc9ad1241e41081c42720fa207139092af
feigege <312012733@qq.com>
2018-11-01 17:36:45 +0800

    --

--- spring-cloud-base/kafka-producer-service/src/main/java/com/KafkaProducerApplication.java
+++ spring-cloud-base/kafka-producer-service/src/main/java/com/KafkaProducerApplication.java
@@ -1,11 +1,11 @@
 package com;
 
-import java.io.IOException;
-import java.util.Properties;
+import java.util.List;
 
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericRecord;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.slf4j.Logger;
@@ -17,12 +17,12 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 import com.config.avro.AvroMessageConfig;
 import com.config.avro.AvroUtil;
-import com.config.kafka.consumer.KafkaConsumerConfig;
-import com.config.kafka.producer.KafkaProducerConfig;
-import com.kafka.consumer.ControlResultConsumer;
-
-import kafka.javaapi.producer.Producer;
-import kafka.producer.KeyedMessage;
+import com.config.kafka.KafkaBean;
+import com.config.kafka.KafkaConfig;
+import com.config.kafka.KafkaUtils;
+import com.kafka.consumer.ControlResultKafkaConsumerThread;
+import com.kafka.consumer.ControlResultPushKafkaConsumerThread;
+import com.utils.ThreadUtils;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -34,98 +34,70 @@ public class KafkaProducerApplication
     {
         
         ConfigurableApplicationContext context = SpringApplication.run(KafkaProducerApplication.class, args);
-        KafkaConsumerConfig kafkaConsumerConfig = context.getBean(ControlResultConsumer.class);
-        KafkaProducerConfig kafkaProducerConfig = context.getBean(KafkaProducerConfig.class);
+        
+        KafkaConfig kafkaConfig = context.getBean(KafkaConfig.class);
+        KafkaBean kafkaBean = context.getBean(KafkaBean.class);
+        KafkaConsumer<String, byte[]> kafkaConsumer = kafkaConfig.kafkaConsumer();
+        
         AvroMessageConfig avroMessageConfig = context.getBean(AvroMessageConfig.class);
+        Schema controlReportSchema = avroMessageConfig.getControlResultReportSchema();
+        Schema controlReportPushSchema = avroMessageConfig.getControlResultReportPushSchema();
         
-        Schema schema = avroMessageConfig.getControlResultSchema();
-        String topic = schema.getNamespace();
+        List<String> topics = kafkaBean.getTopics();
         
-        // ControlResultKafkMsg msg = new ControlResultKafkMsg();
+        for (String topic : topics)
+        {
+            
+            if (topic.equals(controlReportSchema.getName()))
+            {
+                Runnable task = new ControlResultKafkaConsumerThread(topic, controlReportSchema, kafkaConsumer,
+                        context);
+                ThreadUtils.execute(task);
+            }
+            else if (topic.equals(controlReportPushSchema.getName()))
+            {
+                Runnable task = new ControlResultPushKafkaConsumerThread(topic, controlReportPushSchema, kafkaConsumer,
+                        context);
+                ThreadUtils.execute(task);
+            }
+            else
+            {
+                throw new IllegalArgumentException("topicName and schemaName did not match up.");
+            }
+        }
         
-        GenericRecord genericRecord = new GenericData.Record(schema);
-        genericRecord.put("vin", "vin");
-        genericRecord.put("uuid", "uuid");
-        genericRecord.put("time", System.currentTimeMillis());
-        genericRecord.put("result", "true");
+        /***************************************************************/
+        KafkaProducer<String, byte[]> producer = kafkaConfig.kafkaProducer();
         
-//        sendPushMsg(kafkaProducerConfig.getProducer(), schema, topic, genericRecord);
-        send(topic, AvroUtil.bytesWrite(schema, genericRecord));
-        // List<KafkaTopicBean> consumerTopics =
-        // kafkaConsumerConfig.getKafkaConsumerBean().getTopics();
-        //
-        // List<KafkaTopicBean> topics = new ArrayList<>();
-        // topics.addAll(consumerTopics);
-        //
-        // // 自动创建topic
-        // KafkaUtils.createTopics(topics,
-        // kafkaConsumerConfig.getKafkaConsumerBean().getZookeeper());
-        //
-        // List<KafkaConsumerExecutorParam> params = new ArrayList<>();
-        //
-        // for (KafkaTopicBean topic : consumerTopics)
-        // {
-        // KafkaConsumerExecutorParam param = new KafkaConsumerExecutorParam();
-        // KafkaConsumerThreadParam threadParam = new
-        // KafkaConsumerThreadParam();
-        //
-        // param.setKafkaTopic(topic);
-        // threadParam.setKafkaConsumer(kafkaConsumerConfig);
-        // threadParam.setKafkaProducer(kafkaProducerConfig);
-        // threadParam.setAvroMessage(avroMessageConfig);
-        //
-        // if
-        // (topic.getName().equals(avroMessageConfig.getControlResultSchema().getName()))
-        // {
-        // threadParam.setSchema(avroMessageConfig.getControlResultSchema());
-        // threadParam.setThreadClass(ControlResultConsumerThread.class);
-        // }
-        //
-        // else
-        // {
-        // throw new IllegalArgumentException("topicName and schemaName did not
-        // match up.");
-        // }
-        //
-        // param.setThreadParam(threadParam);
-        // params.add(param);
-        // }
-        //
-         kafkaConsumerConfig.executor(params);
+        GenericRecord record = new GenericData.Record(controlReportSchema);
+        record.put("vin", "vin");
+        record.put("uuid", "uuid");
+        record.put("time", System.currentTimeMillis());
+        record.put("result", "true");
         
+        KafkaUtils.produce(controlReportSchema.getName(), producer, AvroUtil.bytesWrite(controlReportSchema, record));
+        
+        //////////////////////////////////////////////////////////////////////////
+        record = new GenericData.Record(controlReportPushSchema);
+        record.put("vin", "vin");
+        record.put("uuid", "uuid");
+        record.put("time", System.currentTimeMillis());
+        record.put("action", 6);
+        record.put("result", "true");
+        
+        KafkaUtils.produce(controlReportPushSchema.getName(), producer,
+                AvroUtil.bytesWrite(controlReportSchema, record));
     }
     
-    static void sendPushMsg(Producer<String, byte[]> sender, Schema sendSchema, String topic,
-            GenericRecord genericRecord) throws IOException
+    public static void send(KafkaProducer<String, byte[]> producer, String topic, byte[] value)
     {
-        LOGGER.info(" send push msg start.  topic:" + topic);
-        
-        byte[] msg = AvroUtil.bytesWrite(sendSchema, genericRecord);
-        sender.send(new KeyedMessage<String, byte[]>(topic, msg));
-        
-        LOGGER.info(" send push msg end. topic:" + topic + ", msg:" + new String(msg));
-    }
-    
-    public static void send( String topic, byte[] value)
-    {
-        Properties properties = new Properties();
-//        properties.put("bootstrap.servers", "192.168.3.62:9092");
-        properties.put("bootstrap.servers", "172.16.5.220:9092");
-        properties.put("acks", "all");
-        properties.put("retries", 0);
-        properties.put("batch.size", 16384);
-        properties.put("linger.ms", 1);
-        properties.put("buffer.memory", 33554432);
-        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        properties.put("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
         try
         {
-            KafkaProducer<String, byte[]> producer = new KafkaProducer<String, byte[]>(properties);
-            producer.send(new ProducerRecord<String, byte[]>(topic,"hello", value));
+            producer.send(new ProducerRecord<String, byte[]>(topic, "hello", value));
         }
         catch (Exception e)
         {
-            e.printStackTrace();
+            LOGGER.error("", e);
             
         }
         finally
