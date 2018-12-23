fafa651375c737f72c7acaf586effe5bcf9f403d
feigege <312012733@qq.com>
2018-12-24 03:49:31 +0800

    d

--- spring-cloud-base/demo-jpa-service/src/main/java/com/DemoJpaServiceApplication.java
+++ spring-cloud-base/demo-jpa-service/src/main/java/com/DemoJpaServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootJpaDemoApplication
+public class DemoJpaServiceApplication
 {
     public static void main(String[] args)
     {
-        SpringApplication.run(SpringbootJpaDemoApplication.class, args);
+        SpringApplication.run(DemoJpaServiceApplication.class, args);
     }
 }
