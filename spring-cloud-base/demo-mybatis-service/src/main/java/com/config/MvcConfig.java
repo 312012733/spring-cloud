ad2822af1de6cfd9e648f1623397be69e4ccb069
feigege <312012733@qq.com>
2018-12-26 04:54:22 +0800

    f

--- spring-cloud-base/demo-mybatis-service/src/main/java/com/config/MvcConfig.java
+++ spring-cloud-base/demo-mybatis-service/src/main/java/com/config/MvcConfig.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
@@ -16,7 +17,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-// @EnableConfigurationProperties(ConfigBean.class)
+@EnableConfigurationProperties(ConfigBean.class)
 public class MvcConfig implements WebMvcConfigurer
 {
     @Autowired
